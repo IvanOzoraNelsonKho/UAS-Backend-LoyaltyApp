665594998854fb09988fcbf86b8747e524272bff
IvanOzoraNelsonKho <Vaannn1806@gmail.com>
2026-05-27 17:47:58 +0700

    Setup resource routing untuk Users, Tiers, dan Missions (tadi file belum di save)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TierController;
+use App\Http\Controllers\MissionController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('users', UserController::class);
+Route::resource('tiers', TierController::class);
+Route::resource('missions', MissionController::class);
