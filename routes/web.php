4cc583e58705a47495f9c436eec7e8a378241303
KennyMartinHoliem <Kennyholiem07@gmail.com>
2026-05-28 01:31:05 +0700

    feat: membuat fitur CRUD untuk model Merchants
    Menambahkan file migration dan Model Merchant. Mengatur route resource. Melengkapi MerchantController dengan logika index, create, store, edit, update, dan destroy. Membuat 3 file tampilan blade (index, create, edit).

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MerchantController;
+
+Route::resource('merchants', MerchantController::class);
 
 Route::get('/', function () {
     return view('welcome');
