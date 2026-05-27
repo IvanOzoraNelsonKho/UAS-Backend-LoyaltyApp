f28958afba6b375699f8fba5a945ce380b020e97
Diana <dianatjo06@gmail.com>
2026-05-27 23:05:45 +0700

    feat: complete MVC and views for Review feature

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PromotionController;
+use App\Http\Controllers\ReviewController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,4 +11,7 @@ Route::get('/', function () {
 Route::get('/promotions', [PromotionController::class, 'index'])->name('promotions.index');
 Route::get('/promotions/create', [PromotionController::class, 'create'])->name('promotions.create');
 Route::post('/promotions', [PromotionController::class, 'store'])->name('promotions.store');
+Route::get('/reviews', [ReviewController::class, 'index'])->name('reviews.index');
+Route::get('/reviews/create', [ReviewController::class, 'create'])->name('reviews.create');
+Route::post('/reviews', [ReviewController::class, 'store'])->name('reviews.store');
 
