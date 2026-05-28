561fef3c4f9897f390f637711f51d0f25357db09
Josephineasn <josephineasnat2@gmail.com>
2026-05-28 18:48:44 +0700

    feat(point-history): implementasi model dan controller untuk mengolah  data mutasi poin

--- app/Http/Controllers/PointHistoryController.php
+++ app/Http/Controllers/PointHistoryController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\PointHistory;
 
 class PointHistoryController extends Controller
 {
-    //
+    public function index()
+    {
+        $currentCustomerId = auth()->id();
+        //buat ambil semua riwayat poin punya customer yang login terus urutin dari paling baru
+        $histories = PointHistory::where('user_id', $currentCustomerId)->latest()->get();
+
+        return view('point_histories.index', compact('histories'));
+    }
 }
