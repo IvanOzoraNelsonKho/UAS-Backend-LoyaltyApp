75ebfd0fa4db4640570f3d1f8080ad2bd7cd2cc1
IvanOzoraNelsonKho <Vaannn1806@gmail.com>
2026-05-28 10:50:05 +0700

    Membuat CRUD dan tampilan HTML untuk tabel Users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Tier;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -12,7 +13,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::all();
+        return view('users.index', compact('users'));
     }
 
     /**
@@ -20,7 +22,8 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        $tiers = Tier::all();
+        return view('users.create', compact('tiers'));
     }
 
     /**
@@ -28,7 +31,22 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'point_balance' => 'required|integer|min:0',
+            'tier_id' => 'required|exists:tiers,id',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt('password'),
+            'point_balance' => $request->point_balance,
+            'tier_id' => $request->tier_id,
+        ]);
+
+        return redirect()->route('users.index');
     }
 
     /**
@@ -44,7 +62,8 @@ class UserController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $tiers = Tier::all();
+        return view('users.edit', compact('user, tier'));
     }
 
     /**
@@ -52,7 +71,21 @@ class UserController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email,' . $user->id,
+            'point_balance' => 'required|integer|min:0',
+            'tier_id' => 'required|exists:tiers,id',
+        ]);
+
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'point_balance' => $request->point_balance,
+            'tier_id' => $request->tier_id,
+        ]);
+
+        return redirect()->route('users.index');
     }
 
     /**
@@ -60,6 +93,7 @@ class UserController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $user->delete();
+        return redirect()->route('users.index');
     }
 }
