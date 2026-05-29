2b2831cd628bd7f36e9654b9e9e40dfb51ea665a
Josephineasn <josephineasnat2@gmail.com>
2026-05-29 21:40:01 +0700

    feat(point-history): buat halaman blade view point history

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,7 @@ use App\Http\Controllers\TransactionController;
 Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
 Route::get('/transactions/create', [TransactionController::class, 'create'])->name('transactions.create');
 Route::post('/transactions', [TransactionController::class, 'store'])->name('transactions.store');
+
+use App\Http\Controllers\PointHistoryController;
+
+Route::get('/point-histories', [PointHistoryController::class, 'index'])->name('point_histories.index');
