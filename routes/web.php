f3cc46c774c0121c318367eeb776dbec1e5de8bb
Josephineasn <josephineasnat2@gmail.com>
2026-06-05 21:13:23 +0700

    feat(referral): implementasi routing dan view blade klaim kode undang teman

--- routes/web.php
+++ routes/web.php
@@ -4,28 +4,42 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PromotionController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\PointHistoryController;
+use App\Http\Controllers\ReferralController;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-use App\Http\Controllers\TransactionController;
+Route::middleware(['auth'])->group(function (){
+    Route::get('/referral/claim', function () {
+            return view('referrals.claim');
+        })->name('referral.claim.form');    
+    Route::post('/referral/claim', [ReferralController::class, 'processReferral'])->name('referral.claim');
 
-Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
-Route::get('/transactions/create', [TransactionController::class, 'create'])->name('transactions.create');
-Route::post('/transactions', [TransactionController::class, 'store'])->name('transactions.store');
+    Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
+    Route::get('/transactions/create', [TransactionController::class, 'create'])->name('transactions.create');
+    Route::post('/transactions', [TransactionController::class, 'store'])->name('transactions.store');
 
-use App\Http\Controllers\PointHistoryController;
+    Route::get('/point-histories', [PointHistoryController::class, 'index'])->name('point_histories.index');
 
-Route::get('/point-histories', [PointHistoryController::class, 'index'])->name('point_histories.index');
+});
+
 Route::get('/promotions', [PromotionController::class, 'index'])->name('promotions.index');
 Route::get('/promotions/create', [PromotionController::class, 'create'])->name('promotions.create');
 Route::post('/promotions', [PromotionController::class, 'store'])->name('promotions.store');
+
 Route::get('/reviews', [ReviewController::class, 'index'])->name('reviews.index');
 Route::get('/reviews/create', [ReviewController::class, 'create'])->name('reviews.create');
 Route::post('/reviews', [ReviewController::class, 'store'])->name('reviews.store');
+
 Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications.index');
 Route::get('/notifications/create', [NotificationController::class, 'create'])->name('notifications.create');
 Route::post('/notifications', [NotificationController::class, 'store'])->name('notifications.store');
 
+// Route::get('/referral/claim', function () {
+//             return view('referrals.claim');
+//         })->name('referral.claim.form');    
+// Route::post('/referral/claim', [ReferralController::class, 'processReferral'])->name('referral.claim');
