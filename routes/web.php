35f6ca8d0b958690dc4435ca18d1fc454832c893
Diana <dianatjo06@gmail.com>
2026-06-06 19:46:16 +0700

    feat: update routes to use resource for promotions

--- routes/web.php
+++ routes/web.php
@@ -31,9 +31,7 @@ Route::post('/transactions', [TransactionController::class, 'store'])->name('tra
 use App\Http\Controllers\PointHistoryController;
 
 Route::get('/point-histories', [PointHistoryController::class, 'index'])->name('point_histories.index');
-Route::get('/promotions', [PromotionController::class, 'index'])->name('promotions.index');
-Route::get('/promotions/create', [PromotionController::class, 'create'])->name('promotions.create');
-Route::post('/promotions', [PromotionController::class, 'store'])->name('promotions.store');
+Route::resource('promotions', PromotionController::class);
 Route::get('/reviews', [ReviewController::class, 'index'])->name('reviews.index');
 Route::get('/reviews/create', [ReviewController::class, 'create'])->name('reviews.create');
 Route::post('/reviews', [ReviewController::class, 'store'])->name('reviews.store');
