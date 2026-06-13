91ce0c1a6dc9da5e3b2360b058a84bbe8b658e9b
andrian <email.github.Andrianhartono06@gmail.com>
2026-06-13 21:37:36 +0700

    full MVC tabel categories, tambah seeder dan form request

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\RewardController;
+use App\Http\Controllers\CartController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('categories', CategoryController::class);
+Route::resource('rewards', RewardController::class);
+Route::resource('carts', CartController::class);
